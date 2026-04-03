8bf0ca5a0914eabade5da1ee09657cecfc9f8677
Matthieu 🇫🇷 <101651928+Mrdolls@users.noreply.github.com>
2026-04-03 17:41:49 +0200

    Create forb_completion.sh

---
+++ forb_completion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+_forb_completions() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="-h --help --json -l --list -e --edit \
+          -P --preset -np --no-preset -gp --get-presets -cp --create-preset \
+          -lp --list-presets -op --open-presets -rp --remove-preset \
+          -b --blacklist -v --verbose -f -p --full-path -a --all --no-auto \
+          -s --source -mlx -lm -t --time --version -up --update --remove"
+    case "$prev" in
+        -f)
+            _filedir
+            return 0
+            ;;
+        -rp|--remove-preset)
+            return 0
+            ;;
+    esac
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+    COMPREPLY=( $(compgen -f -- ${cur}) )
+}
+complete -F _forb_completions forb
