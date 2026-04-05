3777ec584510d9e868e9ae43294615bbdbc254d8
Matthieu 🇫🇷 <101651928+Mrdolls@users.noreply.github.com>
2026-04-05 14:16:22 +0200

    Update install_macOS-beta.sh

--- install_macOS-beta.sh
+++ install_macOS-beta.sh
@@ -27,7 +27,7 @@ log_action "Directories created."
 
 log_action "Fetching core files from GitHub..."
 curl -sfL "$REPO_RAW_URL/forb-beta.sh" -o "$INSTALL_DIR/forb-beta.sh" >> "$LOG_FILE" 2>&1
-curl -sfL "$REPO_RAW_URL/forb-beta_completion.sh" -o "$INSTALL_DIR/forb-beta_completion.sh" >> "$LOG_FILE" 2>&1
+curl -sfL "$REPO_RAW_URL/forb_completion.sh" -o "$INSTALL_DIR/forb-beta_completion.sh" >> "$LOG_FILE" 2>&1
 
 if [ ! -s "$INSTALL_DIR/forb-beta.sh" ]; then
     echo -e "${RED}Error: Could not download forb-beta.sh. Check your internet connection or GitHub repository.${NC}"
